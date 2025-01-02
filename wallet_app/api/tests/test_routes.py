fbe9c04b4bd67da11a8b6e1eaf904e25d5a8e350
Андрей Логвинов <andryu04101999@gmail.com>
2025-01-02 22:47:49 +0300

    add 404

--- wallet_app/api/tests/test_routes.py
+++ wallet_app/api/tests/test_routes.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from rest_framework import status
 
 from .conftest import BaseTestCase
@@ -6,7 +7,16 @@ from .conftest import BaseTestCase
 class TestRoutes(BaseTestCase):
     '''Класс тестов ответов API по GET-запросу.'''
 
-    def test_wallet_balance(self):
+    def test_wallet_get_balance(self):
         '''Тест доступности эндпоинта получения баланса.'''
-        response = self.client.get(self.url_wallet_detail)
-        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        urls_statuses = (
+            (self.url_wallet_detail, status.HTTP_200_OK),
+            (reverse('wallet-detail', args=['non-existent']), status.HTTP_404_NOT_FOUND)
+        )
+        for url, expected_status in urls_statuses:
+            with self.subTest(
+                f'Неверный код статуса по адресу: {url} '
+                f'ожидается {expected_status}'
+            ):
+                response = self.client.get(url)
+                self.assertEqual(expected_status, response.status_code)
